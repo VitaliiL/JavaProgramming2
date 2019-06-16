e5c261829ce6f27654c6cda9814c67ab04e5bf5f
VitaliyLapshakov <vlapshakov@gmail.com>
2019-06-16 10:54:36 +0700

    finished.

--- StackCalculator/src/com/course/lapshakov/sem2/parsers/FileParser.java
+++ StackCalculator/src/com/course/lapshakov/sem2/parsers/FileParser.java
@@ -1,28 +1,56 @@
 package com.course.lapshakov.sem2.parsers;
 
 import com.course.lapshakov.sem2.operations.Command;
+import com.course.lapshakov.sem2.main.UserCommand;
+import com.course.lapshakov.sem2.operations.Print;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class FileParser implements Parser {
     private String inputFile;
-    private static Reader reader = new Reader();
+    private String outputFile;
 
-    public FileParser(String inputFile) {
+    public FileParser(String inputFile, String outputFile) {
         this.inputFile = inputFile;
+        this.outputFile = outputFile;
     }
 
+    @SuppressWarnings("InfiniteLoopStatement")
     @Override
-    public List<Command> getUserCommands() throws IllegalArgumentException {
-        try {
-            return reader.getCommands(new FileInputStream(inputFile));
+    public List<Command> getCommands() {
+        List<Command> commandList = new ArrayList<>();
+        UserCommand userCommand = new UserCommand();
+
+        try (Scanner scanner = new Scanner(new FileInputStream(inputFile));
+             PrintWriter writer = new PrintWriter(outputFile)) {
+
+            while (scanner.hasNextLine()) {
+                String inputScannerString = scanner.nextLine();
+
+                var arguments = inputScannerString.split(" ");
+
+                Command command = userCommand.findNeededCommand(arguments);
+
+                if (command != null) {
+                    commandList.add(command);
+                }
+
+                if ("print".equalsIgnoreCase(inputScannerString)) {
+                    //writer.write();
+                  break;
+                }
+            }
         } catch (FileNotFoundException e) {
             System.err.println("Your file not found.");
             e.printStackTrace();
         }
 
-        throw new IllegalArgumentException("There are no commands.");
+        return commandList;
     }
 }
