bdd644f81de2e719482e55d85a9663a97ce1810c
VitaliyLapshakov <vlapshakov@gmail.com>
2019-06-16 11:04:08 +0700

    finished.

--- StackCalculator/src/com/course/lapshakov/sem2/Parsers/Parser.java
+++
@@ -1,9 +0,0 @@
-package com.course.lapshakov.sem2.parsers;
-
-import com.course.lapshakov.sem2.operations.Command;
-
-import java.util.List;
-
-public interface Parser {
-    List<Command> getCommands();
-}
